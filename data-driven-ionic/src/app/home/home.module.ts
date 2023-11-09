7d3f2d1ecec56aedbe8e57d92630e0dd45c2ac03
Daniel Rovetta <daniel.rovettapassos@gmail.com>
2023-11-09 12:41:36 -0300

    Inciando o template dos dados pessoais

--- data-driven-ionic/src/app/home/home.module.ts
+++ data-driven-ionic/src/app/home/home.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
+
 import { HomePageRoutingModule } from './home-routing.module';
+import { FormDadosPessoaisComponent } from './form-dados-pessoais/form-dados-pessoais.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    FormDadosPessoaisComponent
+  ]
 })
-export class HomePageModule {}
+export class HomePageModule { }
